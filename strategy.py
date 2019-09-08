f60574728c9cc9490d4389767c22790c070b91ec
s-brez <sam@sdbgroup.io>
2019-09-08 22:01:22 +1000

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -12,18 +12,22 @@ class Strategy:
     working data as dataframes."""
 
     ALL_TIMEFRAMES = [
-        "1m", "2m", "3m", "5m", "15m", "30m", "1h", "2h", "3h", "4h",
-        "6h", "8h", "12", "1d", "2d", "3d", "7d", "14d", "28d"]
+        "1Min", "3Min", "5Min", "15Min", "30Min", "1H", "2H", "3H", "4H",
+        "6H", "8H", "12H", "1D", "2D", "3D", "7D", "14D", "28D"]
 
-    TIMEFRAMES_AS_MINUTES = {
-        "1m": 1, "2": 2, "3m": 3, "5m": 5, "15m": 15, "30m": 30, "1h": 60,
-        "2h": 120, "3h": 180, "4h": 240, "6h": 360, "8h": 480, "12": 720,
-        "1d": 1440, "2d": 2880, "3d": 4320, "7d": 10080, "14d": 20160,
-        "28d": 40320}
+    RESAMPLE_KEY = {
+        'open': 'first', 'high': 'max', 'low': 'min',
+        'close': 'last', 'volume': 'sum'}
 
-    MINUTE_TIMEFRAMES = [1, 2, 3, 5, 15, 30]
+    MINUTE_TIMEFRAMES = [1, 3, 5, 15, 30]
     HOUR_TIMEFRAMES = [1, 2, 3, 4, 6, 8, 12]
     DAY_TIMEFRAMES = [1, 2, 3, 7, 14, 28]
+
+    TF_MINS = {
+        "1Min": 1, "3Min": 3, "5Min": 5, "15Min": 15, "30Min": 30, "1H": 60,
+        "2H": 120,
+        "3H": 180, "4H": 240, "6H": 360, "8H": 480, "12H": 720, "1D": 1440,
+        "2D": 2880, "3D": 4320, "7D": 10080, "14D": 20160, "28D": 40320}
 
     def __init__(self, exchanges, logger, db, db_client):
         self.exchanges = exchanges
@@ -32,8 +36,55 @@ class Strategy:
         self.db_client = db_client
         self.db_collections = {
             i.get_name(): db[i.get_name()] for i in self.exchanges}
-        self.dataframes = self.load_dataframes()
         self.models = self.load_models(self.logger)
+
+        # DataFrame container: data[exchange][symbol][timeframe]
+        self.data = {
+            i.get_name(): self.load_data(i) for i in self.exchanges}
+
+    def load_data(self, exchange):
+        """Create and return a dictionary of dataframes for all symbols and
+        timeframes for the given exchange."""
+
+        dicts = {}
+        for symbol in exchange.get_symbols():
+            dicts[symbol] = {
+                tf: self.build_dataframe(
+                    exchange, symbol, tf) for tf in self.ALL_TIMEFRAMES}
+        return dicts
+
+    def build_dataframe(self, exc, sym, tf, lookback=50):
+        """Return a dataframe of size lookback for the given symbol (sym),
+        exchange (exc) and timeframe (tf).
+
+        Lookback is the number of previous bars required by a model to perform
+        to perform its analysis. E.g for a dataframe with tf = 4h, lookback =
+        50, we will need to fetch and resample 4*60*50 1 min bars (12000 bars)
+        into 50 4h bars."""
+
+        # Find the total number of 1min bars needed using TFM dict.
+        size = self.TF_MINS[tf] * lookback
+
+        # Use a projection to remove mongo "_id" field and symbol.
+        result = self.coll.find(
+            {"symbol": sym}, {
+                "_id": 0, "symbol": 0}).limit(
+                    size).sort([("timestamp", -1)])
+
+        # Pass cursor to DataFrame, format time and set index
+        df = pd.DataFrame(result)
+        df['timestamp'] = df['timestamp'].apply(
+            lambda x: datetime.datetime.fromtimestamp(x))
+        df.set_index("timestamp", inplace=True)
+
+        # Downsample 1 min data to target timeframe
+        resampled_df = pd.DataFrame()
+        try:
+            resampled_df = (df.resample(tf).agg(self.RESAMPLE_KEY))
+        except Exception as e:
+            print(e)
+
+        return resampled_df.sort_values(by="timestamp", ascending=False)
 
     def get_relevant_timeframes(self, time):
         """Return a list of timeframes relevant to the just-elapsed period.
