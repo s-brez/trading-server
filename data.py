c526c2ee28445da92c86fb4033546a1c9ab9afc7
s-brez <sam@sdbgroup.io>
2020-02-06 16:52:30 +1100

    Update data.py

--- data.py
+++ data.py
@@ -152,9 +152,14 @@ class Datahandler:
         total_stored = (
             self.db_collections[exchange.get_name()].count_documents({
                 "symbol": symbol}))
-        origin_ts = exchange.get_origin_timestamp(symbol)
-        oldest_ts = result[total_stored - 1]['timestamp']
-        newest_ts = result[0]['timestamp']
+
+        # handle case where there is no existing data (fresh DB)
+        if total_stored == 0:
+            oldest_ts = current_ts
+            newest_ts = current_ts
+        else:
+            oldest_ts = result[total_stored - 1]['timestamp']
+            newest_ts = result[0]['timestamp']
 
         # make timestamps sort-agnostic, in case of sorting mixups
         if oldest_ts > newest_ts:
