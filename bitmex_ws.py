e585d076a4ab8a019fde4b3b6babaf8b7932e8d6
s-brez <sam@sdbgroup.io>
2020-02-20 00:54:34 +1100

    increase data table size in bitmex_ws.py

--- bitmex_ws.py
+++ bitmex_ws.py
@@ -1,38 +1,43 @@
 from time import sleep
+from threading import Thread
 import websocket
-import threading
 import json
 import traceback
 
 
 class Bitmex_WS:
 
-    RECONNECT_TIMEOUT = 10
-
     def __init__(self, logger, symbols, channels, URL, api_key, api_secret):
         self.logger = logger
-        self.data = {}
-        self.keys = {}
         self.symbols = symbols
         self.channels = channels
         self.URL = URL
-
-        # data table size - increase if ticks get cut off during high vol
-        self.MAX_SIZE = 1000 * len(symbols)
-        self.logger.debug("Data table size:" + " " + str(self.MAX_SIZE))
-
         if api_key is not None and api_secret is None:
             raise ValueError('Enter both public and secret keys')
         if api_key is None and api_secret is not None:
             raise ValueError('Enter both public and secret API keys')
         self.api_key = api_key
         self.api_secret = api_secret
+        self.data = {}
+        self.keys = {}
         # websocket.enableTrace(True)
+
+        # data table size - increase if ticks get cut off during high vol.
+        self.MAX_SIZE = 15000 * len(symbols)
+        self.RECONNECT_TIMEOUT = 10
 
         self.connect()
 
     def connect(self):
-        """Start websocket daemon."""
+        """
+        Args:
+            None
+        Returns:
+            Starts the websocket in a thread and connects to subscription
+            channels.
+        Raises:
+            None.
+        """
 
         self.ws = websocket.WebSocketApp(
             self.URL,
@@ -40,10 +45,13 @@ class Bitmex_WS:
             on_error=lambda ws, msg: self.on_error(ws, msg),
             on_close=lambda ws: self.on_close(ws),
             on_open=lambda ws: self.on_open(ws))
-        thread = threading.Thread(target=lambda: self.ws.run_forever())
-        thread.daemon = True
+
+        thread = Thread(
+            target=lambda: self.ws.run_forever(),
+            daemon=True)
         thread.start()
         self.logger.debug("Started websocket daemon.")
+
         timeout = self.RECONNECT_TIMEOUT
         while not self.ws.sock or not self.ws.sock.connected and timeout:
             sleep(1)
@@ -56,6 +64,16 @@ class Bitmex_WS:
                 self.connect()
 
     def on_message(self, ws, msg):
+        """
+        Args:
+            ws: WebSocketApp object
+            msg: message object
+        Returns:
+            Handles incoming websocket messages.
+        Raises:
+            Exception("Unknown")
+        """
+
         msg = json.loads(msg)
         # self.logger.debug(json.dumps(msg))
         table = msg['table'] if 'table' in msg else None
@@ -64,7 +82,6 @@ class Bitmex_WS:
             if 'subscribe' in msg:
                 self.logger.debug(
                     "Subscribed to " + msg['subscribe'] + ".")
-
             elif action:
                 if table not in self.data:
                     self.data[table] = []
@@ -72,14 +89,12 @@ class Bitmex_WS:
             if action == 'partial':
                 self.data[table] = msg['data']
                 self.keys[table] = msg['keys']
-
             elif action == 'insert':
                 self.data[table] += msg['data']
                 # trim data table size when it exceeds MAX_SIZE
                 if(table not in ['order', 'orderBookL2'] and
                         len(self.data[table]) > self.MAX_SIZE):
-                    self.data[table] = self.data[table][self.MAX_SIZE // 2:]  # noqa
-
+                    self.data[table] = self.data[table][self.MAX_SIZE // 2:]
             elif action == 'update':
                 # Locate the item in the collection and update it.
                 for updateData in msg['data']:
@@ -93,7 +108,6 @@ class Bitmex_WS:
                     # Remove cancelled / filled orders
                     if table == 'order' and not self.order_leaves_quantity(item):  # noqa
                         self.data[table].remove(item)
-
             elif action == 'delete':
                 # Locate the item in the collection and remove it.
                 for deleteData in msg['data']:
@@ -109,9 +123,29 @@ class Bitmex_WS:
             self.logger.debug(traceback.format_exc())
 
     def on_open(self, ws):
+        """
+        Args:
+            ws: WebSocketApp object
+        Returns:
+            Invoked when websocket starts. Used to subscribe to channels.
+        Raises:
+            None.
+        """
+
         ws.send(self.get_channel_subscription_string())
 
     def on_error(self, ws, msg):
+        """
+        Args:
+            ws: WebSocketApp object
+            msg: message object
+        Returns:
+            Invoked when websocket encounters an error. Will attempt to
+            reconnect websocket after an error.
+        Raises:
+            None.
+        """
+
         self.logger.debug("BitMEX websocket error: " + str(msg))
 
         # attempt to reconnect if  ws is not connected
@@ -121,26 +155,70 @@ class Bitmex_WS:
         self.connect()
 
     def on_close(self, ws):
+        """
+        Args:
+            ws: WebSocketApp object
+        Returns:
+            Invoked when websocket closes.
+        Raises:
+            None.
+        """
+
         ws.close()
-        pass
 
     def get_orderbook(self):
+        """
+        Args:
+            None.
+        Returns:
+            L2 Orderbook (list).
+        Raises:
+            None.
+        """
+
         return self.data['orderBookL2']
 
     def get_ticks(self):
+        """
+        Args:
+            None.
+        Returns:
+            Ticks (list)
+        Raises:
+            None.
+        """
+
         return self.data['trade']
 
-    def find_item_by_keys(self, keys, table, matchData):
+    def find_item_by_keys(self, keys, table, match_data):
+        """
+        Args:
+            keys: key array object
+            table: data table object
+            match_data: key to match
+        Returns:
+            Finds an item in the data table by the provided key.
+        Raises:
+            None.
+        """
+
         for item in table:
             matched = True
             for key in keys:
-                if item[key] != matchData[key]:
+                if item[key] != match_data[key]:
                     matched = False
             if matched:
                 return item
 
     def get_channel_subscription_string(self):
-        """Return subscription payload for all symbols and channels."""
+        """
+        Args:
+            None.
+        Returns:
+            Subscription payload (string) for all symbols and channels.
+        Raises:
+            None.
+        """
 
         prefix = '{"op": "subscribe", "args": ['
         suffix = ']}'
@@ -156,6 +234,14 @@ class Bitmex_WS:
         return prefix + string + suffix
 
     def order_leaves_quantity(self, o):
+        """
+        Args:
+            o: item to match
+        Returns:
+            True if o['leavesQty'] is zero, False if > 0
+        Raises:
+            None.
+        """
         if o['leavesQty'] is None:
             return True
         return o['leavesQty'] > 0
