4576d0500e7c84346fcb2bb911a80b5a57516c90
s-brez <sam@sdbgroup.io>
2020-02-24 14:27:29 +1100

    add skeleton methods to porfolio.py

--- portfolio.py
+++ portfolio.py
@@ -17,3 +17,51 @@ class Portfolio:
 
     def __init__(self, logger):
         self.logger = logger
+
+    def update_price(self, events, event):
+        """
+        Check price and time updates gainst existing positions.
+
+        Args:
+            events: event queue object.
+            event: new market event.
+
+        Returns:
+           None.
+
+        Raises:
+            None.
+        """
+        pass
+
+    def new_signal(self, events, event):
+        """
+        Process incoming signal event and generate
+
+        Args:
+            events: event queue object.
+            event: new market event.
+
+        Returns:
+           None.
+
+        Raises:
+            None.
+        """
+        pass
+
+    def fill(self, events, event):
+        """
+        Process incoming fill event and update position records accordingly.
+
+        Args:
+            events: event queue object.
+            event: new market event.
+
+        Returns:
+           None.
+
+        Raises:
+            None.
+        """
+        pass
