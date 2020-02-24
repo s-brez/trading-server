199998b0435b4cd3e880e34a56cec2fa5232d375
Sam Breznikar <39972542+s-brez@users.noreply.github.com>
2020-02-24 14:22:20 +1100

    add skeleton feature methods in features.py

--- features.py
+++ features.py
@@ -11,39 +11,71 @@ Some rights reserved. See LICENSE.md, AUTHORS.md.
 
 
 class Features:
-    """Model feature library."""
+    """
+    Model feature library.
+    """
 
-    def trending(lookback_period: int, bars: list):
-        """Return True if price action (bars) forming successive higher or
-        lower swings. Return a -1 for downtrend, 0 for no trend, 1 for
-        uptrend."""
+    def trending(self, lookback_period: int, bars: list):
+        """
+        Identigy if price action (bars) forming successive higher or
+        lower swings.
+        """
 
-        trending = False
-        direction = None
-        return trending, direction
+        return trending
 
-    def convergent(lookback_period: int, bars: list, indicator: list):
+    def new_trend(self, bars: list):
+        """
+        Return True if price has formed a new trend, False if not
+        """
+
+        return new_trend
+
+    def j_curve(self, bars: list):
+        """
+        Identify optimal price action geometry (j-curve) for trends.
+        """
+
+        return j_curve
+
+    def small_bar(self, bars: list, n: int):
+        """
+        Identify if the current bar is "small" relative to the last n bars.
+
+        """
+
+        small_bar
+
+    def reversal_bar(self, bars: list, n: int):
+        """
+        Identify if the last n bars contain a reversal pattern.
+        """
+
+        return reversal_bar
+
+    def convergent(self, lookback_period: int, bars: list, indicator: list):
         """ Return True if price and indicator swings are convergent."""
 
-        convergent = False
         return convergent
 
-    def sr_levels(bars: list):
-        """Return levels of support and resistance in given period."""
+    def sr_levels(self, bars: list):
+        """
+        Return levels of support and resistance in given period.
+        """
 
-        levels = None
         return levels
 
-    def MA(period: int, bars: int):
-        """Simple moving average of previous n bars close price.
+    def MA(self, period: int, bars: int):
+        """
+        Simple moving average of previous n bars close price.
 
-        SMA = (sum of all closes in period) / period. """
+        SMA = (sum of all closes in period) / period.
+        """
 
-        ma = None
         return ma
 
-    def EMA(period: int, bars: list):
-        """Exponential moving average of previous n bars close price.
+    def EMA(self, period: int, bars: list):
+        """
+        Exponential moving average of previous n bars close price.
 
         EMA = price(t) * k + EMA(y) * ( 1 − k )
 
@@ -51,22 +83,24 @@ class Features:
             t = today (current bar for any period)
             y = yesterday (previous bar close price)
             N = number of bars (period)
-            k = 2 / (N + 1) (weight factor)"""
+            k = 2 / (N + 1) (weight factor)
+        """
 
-        ema = None
         return ema
 
-    def MACD(bars: list):
-        """Return MACD for given time series. Bars list must be 26 bars
+    def MACD(self, bars: list):
+        """
+        Return MACD for given time series. Bars list must be 26 bars
         in length (last 26 bars for period).
 
-        MACD = EMA(12) - EMA(26)"""
+        MACD = EMA(12) - EMA(26)
+        """
 
-        macd = None
         return macd
 
-    def CCI(period: int, bars: list):
-        """ Return CCI (Commodity Chanel Index) for n bars close price.
+    def CCI(self, period: int, bars: list):
+        """
+        Return CCI (Commodity Chanel Index) for n bars close price.
 ​
         CCI = (Typical Price − MA) / 0.015 * Mean Deviation
 
@@ -74,7 +108,7 @@ class Features:
             Typical Price = ∑P((H + L + C) / 3))
             P = number of bars (period)
             MA = Moving Average = (∑P Typical Price) / P
-            Mean Deviation=(∑P | Typical Price - MA |) / P"""
+            Mean Deviation=(∑P | Typical Price - MA |) / P
+        """
 
-        cci = None
         return cci
