3cdde48399aab9d9ef639f29dfe21efe2a2de56f
MarcGoulding <gouldingmarc@gmail.com>
2020-02-27 03:59:22 +0000

    added second attempt for sr levels

--- features.py
+++ features.py
@@ -2,6 +2,7 @@ import talib as ta
 import pandas as pd
 import numpy as np
 
+
 class Features:
     """Model feature library. TA indicators are here."""
 
@@ -23,8 +24,10 @@ class Features:
         convergent = False
         return convergent
 
-    def sr_levels(self, bars):
-        """Return:
+    def sr_levels(self, bars, nbounces: int=2, tolerance: int=0.02, window=5):
+        """Inputs:
+
+        Return:
         -- levels of support and resistance in given period.
         -- times the level has been tested (both as a support or as a resistance)
         -- last time it was tested. Older levels are less relevant now.
@@ -33,17 +36,106 @@ class Features:
         1) Obtain fractals to find local maxima an minima
         2) Define distance margin between fractals that will be allowed (they won't be at exactly the same price).
         3) Find fractals located around the same price. Define those as possible sr levels.
-        4) Determine how many fractals are located at that level to determine it's strength.
-        5) ?
+        4) How many fractals are located at that level to determine the num of times it was tested.
+        5) If times tested > nbounces -> it's a valid sr level
+        6) ?
         """
 
-        # TODO: doesn't find sr levels that aren't horizontal.
-        # TODO: does function work properly when no levels are found?
+        # TODO: works properly when no levels are found?
 
         self.check_bars_type(bars)
 
-        levels = None
-        return levels
+        fractals = self.fractals(bars,window=window)
+        # obtain prices for fractals (high if top, low if bottom):
+        # levels = bars.mul(fractals, axis='index')
+        # highs = levels[levels.high > 0].high.values
+        # lows  = levels[levels.low  < 0].low.mul(-1,axis='index').values
+
+        highs = np.multiply(bars.high.values,fractals)
+        highs = highs[highs>0]
+        lows = np.multiply(bars.low.values,fractals)
+        lows = lows[lows<0]*(-1)
+        
+        possible_levels = np.append(highs,lows)
+        
+        # group levels within tolerance % of each other:
+        possible_levels = np.sort(possible_levels)
+        cluster = [possible_levels[0]]
+        k = 0
+        for i in range(1,len(possible_levels)):
+            # if abs(possible_levels[i-1]-possible_levels[i])<tolerance*possible_levels[i-1]:
+            if abs(possible_levels[i-1]-possible_levels[i])<tolerance*max(possible_levels[i-1],possible_levels[i]):
+
+                cluster.append(possible_levels[i])
+
+            else:
+
+                mean = np.mean(cluster)
+                for j in range(k,i):
+                    possible_levels[j] = mean
+                k=i
+                cluster = [possible_levels[i]]
+
+        # remove duplicates:
+        val = np.unique(possible_levels).tolist()
+        # check times each level was tested:
+        levels = []
+        for level in val:
+            if possible_levels.tolist().count(level)>=nbounces:
+                levels.append(level)
+        # return levels
+        return val
+    def supres(ltp, n):
+        """
+        This function takes a numpy array of last traded price
+        and returns a list of support and resistance levels 
+        respectively. n is the number of entries to be scanned.
+        """
+        from scipy.signal import savgol_filter as smooth
+
+        # converting n to a nearest even number
+        if n % 2 != 0:
+            n += 1
+        print(type(int((n / 2))))
+
+        n_ltp = ltp.shape[0]
+
+        # smoothening the curve
+        ltp_s = smooth(ltp, (n + 1), 3)
+
+        # taking a simple derivative
+        ltp_d = np.zeros(n_ltp)
+        ltp_d[1:] = np.subtract(ltp_s[1:], ltp_s[:-1])
+
+        resistance = []
+        support = []
+
+        for i in range(n_ltp - n):
+            arr_sl = ltp_d[i:(i + n)]
+            first = arr_sl[:int((n / 2))]  # first half
+            # first = arr_sl[:4]  # first half
+            last = arr_sl[int((n / 2)):]  # second half
+
+            r_1 = np.sum(first > 0)
+            r_2 = np.sum(last < 0)
+
+            s_1 = np.sum(first < 0)
+            s_2 = np.sum(last > 0)
+
+            # local maxima detection
+            if (r_1 == (n / 2)) and (r_2 == (n / 2)):
+                resistance.append(ltp[i + (int((n / 2)) - 1)])
+
+            # local minima detection
+            if (s_1 == (n / 2)) and (s_2 == (n / 2)):
+                support.append(ltp[i + (int((n / 2)) - 1)])
+
+        levels = np.sort(np.append(support,resistance))
+        tmp_levels = levels
+        for i in range(1,len(levels)):
+        	if levels[i]-levels[i-1]>0.02*levels[i]:
+        		tmp_levels = tmp_levels[tmp_levels!=levels[i]]
+        return support, resistance
 
     def SMA(self, bars, period:int):
         """Simple moving average of previous n (or period) bars close price.
@@ -135,13 +227,15 @@ class Features:
 ​   
         """
         self.check_bars_type(bars)
+        if (window%2 != 1):
+        	window+=1
 
 
         # df.shape[0] is more logical but has a slower runtime, so I went with len(df.index) instead:
         bars_length = len(bars.index)
         frac = np.zeros(bars_length).flatten()
 
-        for bar in range(2,bars_length-2):
+        for bar in range((window-1)/2,bars_length-(window-1)/2):
             if (bars['high'][bar]>bars['high'][bar-2]
                 and bars['high'][bar]>bars['high'][bar-1]
                 and bars['high'][bar]>bars['high'][bar+1]
