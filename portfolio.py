d6efa134704a97392124de9cf7b4d9e549c72f7a
s-brez <sam@sdbgroup.io>
2020-04-21 15:55:11 +1000

    add placeholder methods to portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -9,7 +9,8 @@ Licensed under GNU General Public License 3.0 or later.
 Some rights reserved. See LICENSE.md, AUTHORS.md.
 """
 
-from event import OrderEvent, FillEvent
+from event_types import OrderEvent, FillEvent
+from other_types import Position
 import time
 
 
@@ -22,10 +23,11 @@ class Portfolio:
     Capital allocations to strategies and risk parameters are defined here.
     """
 
-    RISK_PER_TRADE = 1                  # percentage as integer OR 'kelly'
-    MAX_CORRELATED_TRADES = 1
-    MAX_ACCEPTED_DRAWDOWN = 15          # percentage as integer
     MAX_SIMULTANEOUS_POSITIONS = 20
+    MAX_CORRELATED_TRADES = 1
+    MAX_ACCEPTED_DRAWDOWN = 15          # Percentage as integer.
+    RISK_PER_TRADE = 1                  # Percentage as integer OR 'kelly'
+    DEFAULT_STOP = 3                    # % stop distance if none provided.
 
     def __init__(self, exchanges, logger, db_other, db_client, models):
         self.exchanges = exchanges
@@ -35,6 +37,7 @@ class Portfolio:
         self.models = models
 
         self.pf = self.load_portfolio()
+        self.verify_positions()
 
     def update_price(self, events, event):
         """
@@ -66,7 +69,9 @@ class Portfolio:
         Raises:
             None.
         """
-        pass
+
+        if self.within_risk_limits(event):
+            pass
 
     def new_fill(self, events, event):
         """
@@ -89,20 +94,38 @@ class Portfolio:
         Load portfolio matching ID from database.
         """
 
-        portfolio = coll.find_one({"id": ID}, {"_id": 0})
+        portfolio = self.db_other['portfolio'].find_one({"id": ID}, {"_id": 0})
 
         if portfolio:
             return portfolio
         else:
+            # Return a default, empty portfolio if none exists.
             return {
                 'id': ID,
-                'start_date': time.time(),
-                'initial_funds': 10000,
-                'current_value': 10000,
+                'start_date': int(time.time()),
+                'initial_funds': 0,
+                'current_value': 0,
                 'positions': [],
                 'model_allocations': {
-                    "EMA Cross - Testing only": 100},
+                    i: (100 / len(self.models)) for i in self.models},
                 'risk_per_trade': self.RISK_PER_TRADE,
                 'max_correlated_trades': self.MAX_CORRELATED_TRADES,
                 'max_accepted_drawdown': self.MAX_ACCEPTED_DRAWDOWN,
-                'max_simultaneous_positions': self.MAX_SIMULTANEOUS_POSITIONS}
+                'max_simultaneous_positions': self.MAX_SIMULTANEOUS_POSITIONS,
+                'default_stop': self.DEFAULT_STOP}
+
+    def verify_holdings(self):
+        """
+        Check stored portfolio data matches actual positions.
+        """
+
+        # save portfolio to db here, after positions checked.
+
+    def within_risk_limits(self, signal):
+        """
+        Return true if current holdings are within permissible risk limits.
+        """
+
+        # Check if the new signal would breach any risk limits.
+        for position in self.pf['positions']:
+            pass
