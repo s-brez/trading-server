ac32bad1442a06859ac89d543ce8902a97f52fb4
s-brez <sam@sdbgroup.io>
2020-04-26 14:30:58 +1000

    add correlated(), save() method skeletons to porfolio.py, update comments and portfolio fields

--- portfolio.py
+++ portfolio.py
@@ -9,8 +9,8 @@ Licensed under GNU General Public License 3.0 or later.
 Some rights reserved. See LICENSE.md, AUTHORS.md.
 """
 
+from trade_types import SingleInstrumentTrade, Order, Position
 from event_types import OrderEvent, FillEvent
-from trade_types import Position, SingleAssetTrade, Order
 import time
 
 
@@ -20,13 +20,13 @@ class Portfolio:
     and reacting to fill events to open and close positions and strategies
     dictate.
 
-    Capital allocations to strategies and risk parameters are defined here.
+    Capital allocations to strategies and risk parameters defined here.
     """
 
     MAX_SIMULTANEOUS_POSITIONS = 20
     MAX_CORRELATED_TRADES = 1
     MAX_ACCEPTED_DRAWDOWN = 15          # Percentage as integer.
-    RISK_PER_TRADE = 1                  # Percentage as integer OR 'kelly'
+    RISK_PER_TRADE = 1                  # Percentage as integer OR 'KELLY'
     DEFAULT_STOP = 3                    # % stop distance if none provided.
 
     def __init__(self, exchanges, logger, db_other, db_client, models):
@@ -37,7 +37,6 @@ class Portfolio:
         self.models = models
 
         self.pf = self.load_portfolio()
-        self.verify_positions()
 
     def update_price(self, events, event):
         """
@@ -91,42 +90,64 @@ class Portfolio:
 
     def load_portfolio(self, ID=0):
         """
-        Load portfolio matching ID from database.
+        Load portfolio matching ID from database or return an empty default.
         """
 
         portfolio = self.db_other['portfolio'].find_one({"id": ID}, {"_id": 0})
 
         if portfolio:
+            self.verify_portfolio(portfolio)
             return portfolio
         else:
-            # Return a default, empty portfolio if no portfolio exists.
             return {
                 'id': ID,
                 'start_date': int(time.time()),
                 'initial_funds': 0,
                 'current_value': 0,
-                'positions': [],
-                'orders': [],
-                'model_allocations': {
-                    i: (100 / len(self.models)) for i in self.models},
+                'current_drawdown': 0,
+                'trades': [],
+                'model_allocations': {  # Equal allocation as default.
+                    i.get_name(): (100 / len(self.models)) for i in self.models},
                 'risk_per_trade': self.RISK_PER_TRADE,
                 'max_correlated_trades': self.MAX_CORRELATED_TRADES,
                 'max_accepted_drawdown': self.MAX_ACCEPTED_DRAWDOWN,
                 'max_simultaneous_positions': self.MAX_SIMULTANEOUS_POSITIONS,
                 'default_stop': self.DEFAULT_STOP}
 
-    def verify_holdings(self):
+    def verify_portfolio(self, portfolio):
         """
         Check stored portfolio data matches actual positions.
         """
 
-        # save portfolio to db here, after positions checked.
+        # If trades marked active exist (in DB), check their orders and
+        # positions match actual state of trade, update portfoiio if disparate.
+        trades = self.db_other['trades'].find({"active": "True"}, {"_id": 0})
+        if trades:
+
+            venues = [trade['venue'] for trade in trades]
+
+            for venue in venues:
+                
+
+        else:
+            return portfolio
+
+    def save_porfolio(self, portfolio):
+        """
+        Save portfolio state to DB.
+        """
+        pass
 
     def within_risk_limits(self, signal):
         """
         Return true if current holdings are within permissible risk limits.
         """
 
-        # Check if the new signal would breach any risk limits.
-        for position in self.pf['positions']:
+        for trade in self.pf['trades']['exposure']:
             pass
+
+    def correlated(self, instrument):
+        """
+        Return true if active trades are correlated to the given instrument.
+        """
+        pass
