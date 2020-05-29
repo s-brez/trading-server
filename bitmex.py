43926f53cb55432f002b4604f052e878124141d1
s-brez <sam@sdbgroup.io>
2020-05-30 08:41:15 +1000

    add order placement functionality to bitmex.py
    add default request retry behaviour
    add place_single_order and place_bulk_order methods
    update header fields in generate_request_headers method
    add persistent session object instead of generating new session with each order

--- bitmex.py
+++ bitmex.py
@@ -9,7 +9,9 @@ Licensed under GNU General Public License 3.0 or later.
 Some rights reserved. See LICENSE.md, AUTHORS.md.
 """
 
+from requests.packages.urllib3.util.retry import Retry
 from datetime import timezone, datetime, timedelta
+from requests.adapters import HTTPAdapter
 from requests import Request, Session
 from requests.auth import AuthBase
 from urllib.parse import urlparse
@@ -19,6 +21,7 @@ from dateutil import parser
 import traceback
 import requests
 import hashlib
+import json
 import hmac
 import time
 
@@ -30,9 +33,10 @@ class Bitmex(Exchange):
 
     MAX_BARS_PER_REQUEST = 750
     TIMESTAMP_FORMAT = '%Y-%m-%d%H:%M:%S.%f'
+    REQUEST_TIMEOUT = 10
 
     BASE_URL = "https://www.bitmex.com/api/v1"
-    BASE_URL_TESTNET = "https://www.testnet.bitmex.com/api/v1"
+    BASE_URL_TESTNET = "https://testnet.bitmex.com/api/v1"
     WS_URL = "wss://www.bitmex.com/realtime"
     BARS_URL = "/trade/bucketed?binSize="
     TICKS_URL = "/trade?symbol="
@@ -60,6 +64,15 @@ class Bitmex(Exchange):
             "XRPUSD": 1580875200}
 
         self.api_key, self.api_secret = self.load_api_keys()
+
+        # Set default request retry behaviour.
+        retries = Retry(
+            total=5,
+            backoff_factor=0.25,
+            status_forcelist=[502, 503, 504],
+            method_whitelist=False)
+        self.session = Session()
+        self.session.mount('https://', HTTPAdapter(max_retries=retries))
 
         # Non persistent storage for ticks and new 1 mins bars.
         self.bars = {}
@@ -223,7 +236,7 @@ class Bitmex(Exchange):
 
                 # Dont use endTime as it seems to cut off the final few ticks.
                 payload = str(
-                    BASE_URL + TICKS_URL + symbol + "&count=" +
+                    self.BASE_URL + self.TICKS_URL + symbol + "&count=" +
                     "1000&reverse=false&startTime=" + ticks[-1]['timestamp'])
 
                 interim_result = requests.get(payload).json()
@@ -247,28 +260,179 @@ class Bitmex(Exchange):
         return final_ticks
 
     def get_positions(self):
-        s = Session()
         prepared_request = Request(
             'GET',
             self.BASE_URL_TESTNET + self.POSITIONS_URL,
             params='').prepare()
-        request = self.generate_request_headers(prepared_request, api_key,
-                                                api_secret)
-        response = s.send(request).json()
+        request = self.generate_request_headers(prepared_request, self.api_key,
+                                                self.api_secret)
+        response = self.session.send(request).json()
 
         return response
 
     def get_orders(self):
-        s = Session()
         prepared_request = Request(
             'GET',
             self.BASE_URL_TESTNET + self.ORDERS_URL,
             params='').prepare()
-        request = self.generate_request_headers(prepared_request, api_key,
-                                                api_secret)
-        response = s.send(request).json()
+        request = self.generate_request_headers(prepared_request, self.api_key,
+                                                self.api_secret)
+        response = self.session.send(request).json()
 
         return response
+
+    def place_single_order(self, order):
+        payload = self.format_orders([order])[0]
+
+        prepared_request = Request(
+            'POST',
+            self.BASE_URL_TESTNET + self.ORDERS_URL,
+            json=payload,
+            params='').prepare()
+
+        request = self.generate_request_headers(
+            prepared_request,
+            self.api_key,
+            self.api_secret)
+
+        response = self.session.send(request)
+
+        return response
+
+    def place_bulk_orders(self, orders):
+
+        # Separate market orders as BitMEX doesnt allow bulk market orders.
+        m_o = [o for o in orders if o['order_type'] == "MARKET"]
+        nm_o = [o for o in orders if o not in m_o]
+
+        # Send market orders individually amd store responses.
+        responses = [self.place_single_order(o) for o in m_o if m_o]
+
+        # Submit non-market orders in a single batch.
+        response = None
+        if nm_o:
+            payload = {'orders': self.format_orders(nm_o)}
+
+            s = Session()
+
+            prepared_request = Request(
+                'POST',
+                self.BASE_URL_TESTNET + self.BULK_ORDERS_URL,
+                json=payload,
+                params='').prepare()
+
+            request = self.generate_request_headers(
+                prepared_request,
+                self.api_key,
+                self.api_secret)
+
+            response = self.session.send(request)
+
+        # Unpack successful order confirmations and handle errors.
+        order_confirmations = []
+        for r in responses + [response]:
+            if r.status_code == 200:
+
+                res = r.json()
+                if isinstance(res, list):
+                    for item in res:
+                        order_confirmations.append(item)
+                elif isinstance(res, dict):
+                    order_confirmations.append(res)
+
+            elif 401 <= r.status_code <= 404:
+                # Syntax, auth or system limit error messages, raise exception.
+                # Code likely wrong if this occurs.
+                raise Exception(r.status_code, r.json()['error']['message'])
+
+            elif r.status_code == 503:
+                # Server overloaded, retry after 500ms, dont raise exception.
+                print(r.status_code, r.json()['error']['message'])
+
+                # TODO: Check what orders were placed (if any) and re-submit.
+
+            else:
+                print(r.status_code, r.json())
+
+        updated_orders = []
+        if order_confirmations:
+            for res in order_confirmations:
+                for order in orders:
+                    if int(order['order_id']) == int(res['clOrdID']):
+
+                        if res['ordStatus'] == 'Filled':
+                            fill = "FILLED"
+                        elif res['ordStatus'] == 'New':
+                            fill = "NEW"
+
+                        updated_orders.append({
+                            'trade_id': order['trade_id'],
+                            'position_id': order['position_id'],
+                            'order_id': order['order_id'],
+                            'timestamp': res['timestamp'],
+                            'avg_fill_price': res['avgPx'],
+                            'currency': res['currency'],
+                            'venue_id': res['orderID'],
+                            'venue': order['venue'],
+                            'symbol': order['symbol'],
+                            'direction': order['direction'],
+                            'size': res['orderQty'],
+                            'price': res['price'],
+                            'order_type': order['order_type'],
+                            'metatype': order['metatype'],
+                            'void_price': order['void_price'],
+                            'trail': order['trail'],
+                            'reduce_only': order['reduce_only'],
+                            'post_only': order['post_only'],
+                            'batch_size': order['batch_size'],
+                            'status': fill})
+
+        return updated_orders
+
+    def format_orders(self, orders):
+
+        formatted = []
+        for order in orders:
+            price = self.round_increment(order['price'], order['symbol'])
+
+            # TODO: add logic for below three fields.
+            execInst = None
+            stopPx = None
+            timeInForce = None
+
+            symbol = order['symbol']
+            side = "Buy" if order['direction'] == "LONG" else "Sell"
+            orderQty = self.round_increment(order['size'], order['symbol'])
+            clOrdID = order['order_id']
+            text = order['metatype']
+
+            if order['order_type'] == "LIMIT":
+                ordType = "Limit"
+            elif order['order_type'] == "MARKET":
+                ordType = "Market"
+                price = None
+            elif order['order_type'] == "STOP_LIMIT":
+                ordType = "StopLimit"
+            elif order['order_type'] == "STOP":
+                ordType = "Stop"
+                stopPx = price
+                price = None
+            else:
+                ordType = None
+
+            formatted.append({
+                    'symbol': symbol,
+                    'side': side,
+                    'orderQty': orderQty,
+                    'price': price,
+                    'stopPx': stopPx,
+                    'clOrdID': int(order['order_id']),
+                    'ordType': ordType,
+                    'timeInForce': timeInForce,
+                    'execInst': execInst,
+                    'text': text})
+
+        return formatted
 
     def generate_request_signature(self, secret, request_type, url, nonce,
                                    data):
@@ -315,10 +479,13 @@ class Bitmex(Exchange):
             None.
         """
 
-        nonce = str(int(round(time.time()) + 5))
+        nonce = str(int(round(time.time()) + self.REQUEST_TIMEOUT))
         request.headers['api-expires'] = nonce
-        request.headers['api-key'] = api_key
-        request.headers['api-signature'] = generate_request_signature(
-            api_secret, request.method, request.url, nonce, request.body or '')
+        request.headers['api-key'] = self.api_key
+        request.headers['api-signature'] = self.generate_request_signature(
+            self.api_secret, request.method, request.url, nonce, request.body or '')  # noqa
+        request.headers['Content-Type'] = 'application/json'
+        request.headers['Accept'] = 'application/json'
+        request.headers['X-Requested-With'] = 'XMLHttpRequest'
 
         return request
