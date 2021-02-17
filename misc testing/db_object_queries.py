aa0d27dcb9481bcb5782f85673318ae3f9fa98f1
Sam Breznikar <sam@sdbgroup.io>
2021-02-17 22:51:56 +1100

    Update db_object_queries.py

--- misc testing/db_object_queries.py
+++ misc testing/db_object_queries.py
@@ -12,6 +12,12 @@ coll = db_other['portfolio']
 
 # result = coll.find({}, {"_id": 0}).sort([("entry_timestamp", -1)])  # signals
 result = list(coll.find({}, {"_id": 0}).sort([("id", -1)]))  # portfolio
-# result = coll.find({}, {"_id": 0}).sort([("trade_id", -1)])  # trades
+# result = coll.find({"trade_id": 1}, {"_id": 0}).sort([("trade_id", -1)])  # trades
+# coll.update_one({"trade_id": 5}, {"$set": {"consent": True}})
+# result = coll.find_one({"trade_id": 1}, {"_id": 0})  # trades
 
 [print((json.dumps(i, indent=2))) for i in result]
+
+# print(json.dumps(result, indent=2))
+
+
